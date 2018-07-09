59fce00d36e4746c0f411cbe29d81d1af5ed62a2
zhangxiang93 <zhangxiang93@163.com>
2018-07-09 18:31:55 +0800

    up

--- spiderWeekly/spiders/juejin.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-export PATH=$PATH:/usr/local/bin
-# 跳转至Scrapy项目目录
-cd /home/zxiangxiang/spiderWeekly/spiderWeekly/spiders
-# 后台运行抓取，并将日志输出到run.log文件
-nohup scrapy crawl juejin >> logs/zhihu.log 2>&1 &
